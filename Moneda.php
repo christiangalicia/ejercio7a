8475ebf6a4255a619004e1efb07725934551d0ab
christian <christian@LAPTOP-PPJ6KL08>
2017-09-05 15:49:34 -0500

    se programo el metodo para actualizar el valor de la monenda

--- Moneda.php
+++ Moneda.php
@@ -28,4 +28,13 @@ class Moneda {
         echo $this->valor;
     }
     
+    /**
+     * metodo que me permie cambiar el valor
+     * @param String $valor <p>
+     * valor para iniciar la moneda
+     * @return void
+     */
+    public function cambiarValor($valor){
+        $this->valor= $valor;
+    }
 }
